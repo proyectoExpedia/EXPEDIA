721cc803927a4aff49e3f2ff0c1cfd1d3578825a
Daruwarchief <pabquecha@gmail.com>
2016-02-16 17:21:11 -0600

    Para el login de cada uno

--- EXPEDIA/EXPEDIA/Conexion.cs
+++ EXPEDIA/EXPEDIA/Conexion.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Data;
 using System.Data.Sql;
 using System.Data.SqlClient;
+using System.Security;
 
 
 namespace EXPEDIA
@@ -14,7 +15,10 @@ namespace EXPEDIA
         public SqlConnection Conectar(){
             try{
                 SqlConnection Conect;
-                Conect = new SqlConnection("Data Source=ROJAS-PC\\SQLEXPRESS;Initial Catalog=BD_EXPEDIA;User ID=Administrador;Password=123456");
+                //Conect = new SqlConnection("Data Source=DARUWARCHIEF-PC\\SQLEXPRESS;Initial Catalog=BD_EXPEDIA;User ID=root;Password=root123");
+                /// Conect = new SqlConnection("Data Source=DARUWARCHIEF-PC\\SQLEXPRESS;Initial Catalog=BD_EXPEDIA;User ID=root;Password=root123");
+                /// Conect = new SqlConnection("Data Source=DARUWARCHIEF-PC\\SQLEXPRESS;Initial Catalog=BD_EXPEDIA;User ID=root;Password=root123");
+                Conect = new SqlConnection("Data Source=DARUWARCHIEF-PC\\SQLEXPRESS;Initial Catalog=BD_EXPEDIA;User ID=root;Password=root123");
                 return Conect;
             }catch(Exception ){
                 return null;
