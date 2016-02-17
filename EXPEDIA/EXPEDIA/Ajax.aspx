dc73f35e7b09fc1e97261944b6c50f335d7d4b75
proyectoExpedia <proyectoexpedia@gmail.com>
2016-02-17 14:22:53 +0100

    Ajax

---
+++ EXPEDIA/EXPEDIA/Ajax.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Ajax.aspx.cs" Inherits="EXPEDIA.Ajax" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+        <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+        <ContentTemplate>
+            <div>
+                <asp:TextBox ID="Ingresa" runat="server"></asp:TextBox>
+                <asp:Button ID="BtnAjax" OnClick="BtnAjax_Click" runat="server" Text="Button" />
+                <asp:TextBox ID="Devuelve" runat="server"></asp:TextBox>
+            </div>
+        </ContentTemplate>
+        </asp:UpdatePanel>
+    </form>
+</body>
+</html>
---
+++ EXPEDIA/EXPEDIA/Ajax.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace EXPEDIA
+{
+    public partial class Ajax : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnAjax_Click(object sender, EventArgs e) {
+            Conexion c = new Conexion();
+            SqlConnection Conexion = c.Conectar();
+            string Sql = @"SELECT bd_nombre FROM Usuarios WHERE bd_cedula = @user";
+            Conexion.Open();//abrimos conexion
+            SqlCommand cmd = new SqlCommand(Sql, Conexion); //ejecutamos la instruccion
+            cmd.Parameters.AddWithValue("@user", Ingresa.Text);
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                while (reader.Read()){
+                    Devuelve.Text = reader.GetString(0);
+                }
+            }
+            Conexion.Close();
+        }
+    }
+}
