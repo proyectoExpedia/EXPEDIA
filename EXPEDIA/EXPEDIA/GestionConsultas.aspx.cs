6bc87fb0986dc910c4cb641608e93a725853d4f6
CRONOS-19 <japach1994@gamil.com>
2016-02-23 23:06:05 -0600

    CONSULTA A LA BASE DE DATOS Y EXPORTA A EXCEL

--- EXPEDIA/EXPEDIA/GestionConsultas.aspx.cs
+++ EXPEDIA/EXPEDIA/GestionConsultas.aspx.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 
 namespace EXPEDIA
@@ -13,5 +18,84 @@ namespace EXPEDIA
         {
 
         }
+
+
+        protected void Consultar_Click(object sender, EventArgs e)
+        {
+            DataTable dt = new DataTable();
+
+            dt.Columns.AddRange(new DataColumn[11] {
+                               new DataColumn("_proveedor", typeof(string)),
+                               new DataColumn("_descripcion_activo", typeof(string)),
+                            new DataColumn("Placa", typeof(string)),
+                            new DataColumn("Serie",typeof(string)),
+                             new DataColumn("bd_tipo_activo",typeof(string)),
+                              new DataColumn("fecha_garantia_activo",typeof(string)),
+                               new DataColumn("departamento",typeof(string)),
+                                new DataColumn("especificacion_tecnica",typeof(string)),
+                                 new DataColumn("duracion_de_contrato",typeof(string)),
+                                  new DataColumn("fecha_compra",typeof(string)),
+                                   new DataColumn("costo_activo",typeof(string)),
+
+
+
+            });
+
+
+            try
+            {
+                string num = numero.Text;
+                Conexion c = new Conexion();
+                SqlConnection Conexion = c.Conectar();
+                string Sql = @"SELECT * FROM Activos  WHERE bd_numero_placa= @num OR bd_numero_serie = @num";
+                Conexion.Open();//abrimos conexion
+                SqlCommand cmd = new SqlCommand(Sql, Conexion); //ejecutamos la instruccion
+                cmd.Parameters.AddWithValue("@num", num); //enviamos los paramet
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+
+                        dt.Rows.Add(reader.GetInt16(0).ToString(), reader.GetString(1), reader.GetString(2), reader.GetDateTime(3).ToString(), reader.GetString(4), reader.GetString(5), reader.GetString(6), reader.GetString(7), reader.GetDateTime(8).ToString(), reader.GetDateTime(9).ToString(), reader.GetInt32(10).ToString());
+                    }
+
+                    lista.DataSource = dt;
+                    lista.DataBind();
+                }
+            }
+            catch (Exception a) { Response.Write(a); }
+        }
+
+      
+
+
+        protected void exportar_Click(object sender, EventArgs e)
+        {
+            ExportToExcel("Informe.xls", lista);
+        }
+
+        private void ExportToExcel(string nameReport, GridView wControl)
+        {
+            HttpResponse response = Response;
+            StringWriter sw = new StringWriter();
+            HtmlTextWriter htw = new HtmlTextWriter(sw);
+            Page pageToRender = new Page();
+            HtmlForm form = new HtmlForm();
+            form.Controls.Add(wControl);
+            pageToRender.Controls.Add(form);
+            response.Clear();
+            response.Buffer = true;
+            response.ContentType = "application/vnd.ms-excel";
+            response.AddHeader("Content-Disposition", "attachment;filename=" + nameReport);
+            response.Charset = "UTF-8";
+            response.ContentEncoding = Encoding.Default;
+            pageToRender.RenderControl(htw);
+            response.Write(sw.ToString());
+            response.End();
+        }
+
+
     }
 }
--- EXPEDIA/EXPEDIA/GestionConsultas.aspx.designer.cs
+++ EXPEDIA/EXPEDIA/GestionConsultas.aspx.designer.cs
@@ -11,5 +11,50 @@ namespace EXPEDIA {
     
     
     public partial class GestionConsultas {
+        
+        /// <summary>
+        /// Control for.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm @for;
+        
+        /// <summary>
+        /// Control numero.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox numero;
+        
+        /// <summary>
+        /// Control Button1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
+        /// Control lista.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView lista;
+        
+        /// <summary>
+        /// Control exportar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button exportar;
     }
 }
